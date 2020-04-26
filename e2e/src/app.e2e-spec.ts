cb2c07f51e8bb0e6736a7c4f174e7374a9e49a22
isubham <subhamkumarchandrawansi@gmail.com>
2020-04-26 11:22:36 +0530

    Project Setup #1

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to lazy-loading!');
+    expect(page.getParagraphText()).toEqual('Welcome to pitavya-web');
   });
 });
